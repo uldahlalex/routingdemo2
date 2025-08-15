f695b0462edd1aa6f5f4b0f4569dc456fff2a392
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-08-15 12:24:41 +0200

    added the authors detail page

--- src/AuthorDetail.tsx
+++ src/AuthorDetail.tsx
@@ -1,12 +1,36 @@
 import {useParams} from "react-router";
+import {useEffect, useState} from "react";
+
+export type AuthorDetailParam = {
+    authorId: string;
+}
+export interface Author {
+    id: number
+    idBook: number
+    firstName: string
+    lastName: string
+}
+
 
 export default function AuthorDetail() {
     
-    const params = useParams();
+    const params = useParams<AuthorDetailParam>();
+    const [currentAuthor, setCurrentAuthor] = useState<Author>()
+    
+    useEffect(() => {
+        fetch('https://fakerestapi.azurewebsites.net/api/v1/Authors/'+params.authorId)
+            .then(result => {
+                result.json().then(json => {
+                    setCurrentAuthor(json)
+                })
+            })
+    }, [])
     
     return <div>
 
-        {JSON.stringify(params)}
+        {
+            JSON.stringify(currentAuthor)
+        }
         
     </div>
 }
